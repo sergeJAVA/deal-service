f73d12ad4cf0028bcf93eded2f97d62d6102c291
Sergey Kurochkin <serega.curo4kin2014@gmail.com>
2025-06-28 14:11:21 +0300

    first commit

---
+++ src/test/java/com/example/deal_service/DealServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
